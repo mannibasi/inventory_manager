0e7f188342728e5481c5691dcac8b67558aa209c
Manni Basi <manni.basi@gmail.com>
2021-09-12 14:16:24 +0100

    Added 'red' integration tests and updated README

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# inventory_manager
-Simple inventory management app built with pytest
+# Simple inventory management app built with pytest
+## Developed with [pytest](https://docs.pytest.org/en/latest/), a mature full-featured Python testing framework
+
+```
+python -m venv env
+. env/Scripts/activate
+
+pip install --upgrade pip
+pip install pytest
+pip install coverage
+pip install pytest-cov
+```
+```
+pytest
+pytest --cov=primes
+```
+
+See related TDD blog here:
+https://stackabuse.com/test-driven-development-with-pytest/
---
+++ test_inventory.py
@@ -0,0 +1,16 @@
+def test_buy_and_sell_nikes_adidas():
+    inventory = Inventory()
+    assert inventory.limit == 100
+    assert inventory.total_items == 0
+
+    inventory.add_new_stock('Nike Sneakers', 50.00, 10)
+    assert inventory.total_items == 10
+
+    inventory.add_new_stock('Adidas Sweatpants', 70.00, 5)
+    assert inventory.total_items == 15
+
+    inventory.remove_stock('Nike Sneakers', 2)
+    assert inventory.total_items == 13
+
+    inventory.remove_stock('Adidas Sweatpants', 1)
+    assert  inventory.total_items == 12
