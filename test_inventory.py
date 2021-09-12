b51a43becb2119086357feae8156ff6a5a325665
Manni Basi <manni.basi@gmail.com>
2021-09-12 14:28:50 +0100

    Added stock inventory fixture

--- test_inventory.py
+++ test_inventory.py
@@ -1,4 +1,5 @@
 from inventory import *
+import pytest
 
 
 def test_buy_and_sell_nikes_adidas():
@@ -29,3 +30,15 @@ def test_custom_inventory_limit():
     inventory = Inventory(limit=25)
     assert inventory.limit == 25
     assert inventory.total_items == 0
+
+
+@pytest.fixture
+def no_stock_inventory():
+    return Inventory(10)
+
+
+def test_add_new_stock_success(no_stock_inventory):
+    no_stock_inventory.add_new_stock('Test Jacket', 10.00, 5)
+    assert no_stock_inventory.total_items == 5
+    assert no_stock_inventory.stocks['Test Jacket']['price'] == 10.00
+    assert no_stock_inventory.stocks['Test Jacket']['quantity'] == 5
